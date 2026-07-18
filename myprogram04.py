2f4c15803eefcbb988c03de6f19e3a20b9078521
Jirayut09120 <467025@wsk.ac.th>
2026-07-18 03:10:51 +0000

    myprogram04.py

---
+++ myprogram04.py
@@ -0,0 +1,8 @@
+print("=== โปรแกรมคำนวณพื้นที่สามเหลี่ยม ===")
+
+base = float(input("กรอกฐาน: "))
+height = float(input("กรอกความสูง: "))
+
+area = 0.5 * base * height
+
+print("พื้นที่สามเหลี่ยม =", area)
